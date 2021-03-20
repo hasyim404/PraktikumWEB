5f7c739cd17573273a9afd391c67b869bed036f2
Muhammad Ismail <72483245+hasyim404@users.noreply.github.com>
2021-03-20 22:03:03 +0700

    Update index.php

--- Semester2/Praktikum02 (PHP02)/If else/index.php
+++ Semester2/Praktikum02 (PHP02)/If else/index.php
@@ -1,99 +1,88 @@
-<?php 
-$nama=$_POST['nama'];
-$matkul=$_POST['matkul'];
-$uts=$_POST['uts'];
-$uas=$_POST['uas'];
-$tugas=$_POST['tugas'];
-?>
-
-<?php
-$nilai_akhir = (30*$uts/100 + 35*$uas/100 + 35*$tugas/100);
-if ($nilai_akhir >= 55 && $nilai_akhir <= 100){
-    $grade_na='Lulus';
-}else {
-    $grade_na='Tidak Lulus';
-}
-
-// Grade Nilai Akhir
-if ($nilai_akhir >= 85 && $nilai_akhir <= 100){
-    $grade_nilai='A';
-}
-elseif ($nilai_akhir >= 70){
-    $grade_nilai='B';
-}
-elseif ($nilai_akhir >= 56){
-    $grade_nilai='C';
-}
-elseif ($nilai_akhir >= 36){
-    $grade_nilai='D';
-}
-elseif ($nilai_akhir >= 0){
-    $grade_nilai='E';
-}
-else {
-    $grade_nilai='I';
-}
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Nilai Siswa</title>
+    <title>Form Pengisian Nilai</title>
+    <link rel="icon" type="image/png" href="https://i.postimg.cc/9fnxvrDS/formlogo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <style>
-        body{
-            padding: 30px;
-        }
-        .btnn {
-            padding-top: 50px;
-        }
-        button {
-            box-shadow: 0 0 2px rgba(0, 0, 0, 1);
 
-        }
-    </style>
 </head>
+<style>
+    body{
+        background-color: #f2f2f2;
+        padding-top: 5rem;
+        }
+    .mid{
+        text-align: center;
+        margin: 5px;
+    }
+    .box{
+        background-color: white;
+        border-radius: 4px;
+        box-shadow: 0 0 1px rgba(0, 0, 0, 0.5);
+    }
+    .btt{
+        align-items: center;
+        margin: 20px;
+    }
+    #judul {
+        padding-bottom: 20px;
+    }
+    .rainer {
+        padding: 20px;
+    }
+</style>
 <body>
-    <div class="container">
-        <div class="table-responsive">
-            <table class="table table-default table-hover">
-            <thead>
-                <tr>
-                <th>NO</th>
-                <th>Nama</th>
-                <th>Mata Kuliah</th>
-                <th>UTS</th>
-                <th>UAS</th>
-                <th>Tugas</th>
-                <th>Nilai Akhir</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $nomor=1;
-                    echo '<tr>
-                    <td>'.$nomor.'</td>';
-                    echo '<td>'.$nama.'</td>';
-                    echo '<td>'.$matkul.'</td>';
-                    echo '<td>'.$uts.'</td>';
-                    echo '<td>'.$uas.'</td>';
-                    echo '<td>'.$tugas.'</td>';
-                    echo '<td>'.$grade_nilai.' '.'('.$nilai_akhir.')'.'</td>';
-                    echo '</tr>';
-                ?>
-            </tbody>
-            </table>
+    <div class="container box">
+        <form method="post" action="conditional_ifelse.php">
+        <div class="d-flex flex-column bd-highlight mb-3">
+        
+    
         </div>
-        <?php echo '<h5>'.strtoupper($nama).' DINYATAKAN:</h5>'.$grade_na;  ?>
-        <div class="col text-right btnn">
-            <a href="index.php"><button type="submit" class="btn btn-primary btt">Kembali</button></a>
+        <div class="rainer">
+            <h3 id="judul">Form Nilai Siswa</h3>
+            <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="nama" required placeholder="Nama">
+            </div>
+            <div class="form-group">
+                <label>Mata Kuliah</label>
+                    <div class="col-md-4">
+                        <select class="form-select" name="matkul" aria-label="" required>
+                        <option selected></option>
+                        <option value="PPKN">PPKN</option>
+                        <option value="Bahasa Inggris">Bahasa Inggris</option>
+                        <option value="Keterampilan Komunikasi">Keterampilan Komunikasi</option>
+                        <option value="Statistik">Statistik</option>
+                        <option value="Basis Data">Basis Data</option>
+                        <option value="UI/UX">UI/UX</option>
+                        <option value="Pemrograman Web 2">Pemrograman Web 2</option>
+                        <option value="Jaringan Komputer">Jaringan Komputer</option>
+                    </div>
+                </div>
+                </select>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="form-group mid">
+                    <label>Nilai UTS</label>
+                    <input type="text" class="form-control" name="uts" required placeholder="Nilai UTS">
+                </div>
+                <div class="form-group mid">
+                    <label>Nilai UAS</label>
+                    <input type="text" class="form-control" name="uas" required placeholder="Nilau UAS">
+                </div>
+                <div class="form-group mid">
+                    <label>Nilai Tugas</label>
+                    <input type="text" class="form-control" name="tugas" required placeholder="Nilai Tugas/Praktikum">
+                </div>
+            </div>
+            <div class="col text-center">
+                <button type="submit" class="btn btn-primary btt">Submit Nilai</button>
+            </div>
+        </form>
         </div>
     </div>
 </body>
 </html>
-
