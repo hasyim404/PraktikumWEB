f9ce8da80f976461a7cc4ea1117a484b8635ca96
Muhammad Ismail <72483245+hasyim404@users.noreply.github.com>
2021-04-04 22:47:04 +0700

    Update daftar_bmipasien.php

--- Semester2/Praktikum03 (OOP)/daftar_bmipasien.php
+++ Semester2/Praktikum03 (OOP)/daftar_bmipasien.php
@@ -1,6 +1,13 @@
 <?php
-require_once 'class_bmipasien.php';
+    require 'class_bmipasien.php';
 
+    $data_bmi = [
+        ['nama' => 'Ismaill', 'umur' => 19, 'jk' => 'Lakilaki', 'berat' => 61, 'tinggi' => 171],
+        ['nama' => 'Barbara', 'umur' => 15, 'jk' => 'Perempuan', 'berat' => 43, 'tinggi' => 160],
+        ['nama' => 'Barbatros', 'umur' => 28, 'jk' => 'Laki-laki', 'berat' => 63, 'tinggi' => 150],
+        ['nama' => 'Barbarian', 'umur' => 45, 'jk' => 'Laki-laki', 'berat' => 80, 'tinggi' => 181],
+    ];
+    $nomor=1;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +19,11 @@ require_once 'class_bmipasien.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
 </head>
+<style>
+body {
+    padding: 2rem;
+}
+</style>
 <body>
 <div class="container">
     <table class="table table-hover table-responsive">
@@ -28,10 +40,25 @@ require_once 'class_bmipasien.php';
         </tr>
     </thead>
     <tbody>
-        <tr>
+        <?php
+            foreach ($data_bmi as $tablebmi) {
+                $bmi1 = new BmiPasien($tablebmi['nama'], $tablebmi['umur'], $tablebmi['jk'], $tablebmi['berat'], $tablebmi['tinggi']);
+                $hasil = $bmi1->hasilBMI();
+                $status = $bmi1->statusBMI();
 
+                echo '<tr><td>'.$nomor.'</td>';
+                echo '<td>'.$bmi1->nama.'</td>';
+                echo '<td>'.$bmi1->jk.'</td>';
+                echo '<td>'.$bmi1->umur.'</td>';
+                echo '<td>'.$bmi1->berat.'</td>';
+                echo '<td>'.$bmi1->tinggi.'</td>';
+                echo '<td>'.$hasil.'</td>';
+                echo '<td>'.$status.'</td>';
+                echo '</tr>';
+                $nomor++;
+            }
+        ?>
 
-        </tr>
     </tbody>
     </table>
 </div>
